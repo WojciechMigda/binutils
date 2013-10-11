c95c4bcc57642d6da8322d3c5f7b3b2f143c5dcc
Wojciech Migda <wojtek.golf@interia.pl>
2013-10-11 19:09:09 +0200

    tic4x clone, objdump read objs, incorrectly though
    Signed-off-by: Wojciech Migda <wojtek.golf@interia.pl>

---
+++ include/coff/tic6x.h
@@ -0,0 +1,50 @@
+/* TI COFF information for Texas Instruments TMS320C6X.
+   This file customizes the settings in coff/ti.h. 
+   
+   Copyright 2002, 2003, 2010, 2013 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston,
+   MA 02110-1301, USA.  */
+
+#ifndef COFF_TIC6X_H
+#define COFF_TIC6X_H
+
+#define TIC6X_TARGET_ID 	0x0099
+/* Octets per byte, as a power of two.  */
+#define TI_TARGET_ID 		TIC6X_TARGET_ID
+#define OCTETS_PER_BYTE_POWER 	2 
+/* Add to howto to get absolute/sect-relative version.  */
+#define HOWTO_BANK 		6		
+#define TICOFF_TARGET_ARCH 	bfd_arch_tic6x
+/* We use COFF2.  */
+#define TICOFF_DEFAULT_MAGIC 	TICOFF2MAGIC 
+
+#if 0
+#define TICOFF_TARGET_MACHINE_GET(FLAGS) \
+  (((FLAGS) & F_VERS) ? bfd_mach_tic4x : bfd_mach_tic3x)
+
+#define TICOFF_TARGET_MACHINE_SET(FLAGSP, MACHINE)	\
+  do							\
+    {							\
+      if ((MACHINE) == bfd_mach_tic6x)			\
+        *(FLAGSP) |= F_VERS;				\
+    }							\
+  while (0)
+#endif
+
+#include "coff/ti.h"
+
+#endif /* COFF_TIC6X_H */
+
